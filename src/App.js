a61949452a72aac47696643b554bcf210f02ef81
Oscar Corcho <ojc8818@gmail.com>
2024-02-29 17:02:26 -0500

    (chore:start) initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { FileInput as NewFileInput } from '8base-file-manager'
 
-function App() {
+const App = ({ client }) => {
+  const handleClickModal = (openModal) => {
+    console.log('clicked')
+    openModal();
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <NewFileInput 
+      apiKey={ `43bea5cf-fa8a-4924-ac3a-447cfd64c648` }
+      onChange={ (file) => console.log(file) }
+      useFilestack={ false }
+      maxFiles={ 1 }
+      value={ null }
+      workspace={ `clt4ho3cr00000akvgsqj51qg` }
+      uploadHost={ `https://qa4-file-manager.8basedev.com` }
+    >
+      { ({ openModal }) => (
+        <>
+          <button onClick={ ()  => handleClickModal(openModal) }>Button Text</button>
+        </>
+      ) }
+    </NewFileInput>
+  )
+};
 
-export default App;
+export default App ;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,40 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+import { ApolloProvider } from "@apollo/client";
+import { setContext } from '@apollo/client/link/context';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+const API_TOKEN = '43bea5cf-fa8a-4924-ac3a-447cfd64c648'
+
+// Create a middleware that adds the API token to the request headers
+const authLink = setContext((_, { headers }) => {
+  return {
+    headers: {
+      ...headers,
+      authorization: API_TOKEN ? `Bearer ${API_TOKEN}` : '',
+    },
+  };
+});
+
+const httpLink = createHttpLink({
+  uri: `https://qa4-api.8basedev.com/clt4ho3cr00000akvgsqj51qg`,
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
+ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <ApolloProvider client={ client }>
+      <App />
+    </ApolloProvider>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
