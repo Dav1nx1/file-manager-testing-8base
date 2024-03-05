4a6aad1a86d59b2f0ddb824c642e218dee118398
Oscar Corcho <ojc8818@gmail.com>
2024-03-05 13:08:43 -0500

    changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
-import React from 'react';
-import { FileInput as NewFileInput } from '8base-file-manager'
+import React, { useState } from 'react';
+import { FileInput as NewFileInput } from '@8base/file-manager'
+import { gql, useMutation } from '@apollo/client'
+
+const MY_MUTATION = gql`
+  mutation PruebaCreate($data: PruebaCreateInput!) {
+    pruebaCreate(data: $data) {
+      id
+      info {
+        id
+        fileId
+        filename
+        downloadUrl
+        shareUrl
+        meta
+        public
+        __typename
+      }
+      _description
+      __typename
+    }
+  }
+
+`;
 
 const App = ({ client }) => {
-  const handleClickModal = (openModal) => {
-    console.log('clicked')
-    openModal();
+  const [nameFile, setnameFile] = useState("Name file..");
+  const [fileId, setfileId] = useState(null);
+
+  const [createRow, { dataCreate }] = useMutation(MY_MUTATION);
+  const handleCreateRow = () => {
+    createRow({
+      variables: {
+        data: {
+          info: {
+            create: {
+              fileId: fileId,
+              filename: nameFile,
+              public: false,
+            },
+          },
+        },
+      },
+    });
   };
+
+  
+  const handleClickModal = async (pick) => {
+    const accept = undefined
+    await pick({
+      ...(accept ? { accept } : {}),
+    });
+  };
+
+  const handleGetResponse = (file) => {
+    setnameFile(file[0].filename);
+    setfileId(file[0].fileId);
+  };
+
+  if (dataCreate) console.log("dataCreate: " + dataCreate);
 
   return (
     <NewFileInput 
       apiKey={ `43bea5cf-fa8a-4924-ac3a-447cfd64c648` }
-      onChange={ (file) => console.log(file) }
+      // onChange={ (file) => alert(`Uploaded File`) }
+      onChange={handleGetResponse}
       useFilestack={ false }
-      maxFiles={ 1 }
+      maxFiles={ 5 }
       value={ null }
+      environment={ `Master` }
       workspace={ `clt4ho3cr00000akvgsqj51qg` }
       uploadHost={ `https://qa4-file-manager.8basedev.com` }
     >
-      { ({ openModal }) => (
+      { ({ pick }) => (
         <>
-          <button onClick={ ()  => handleClickModal(openModal) }>Button Text</button>
+          <button onClick={ ()  => handleClickModal(pick) }>Button Text</button>
+          <button onClick={ () => handleCreateRow() }> Create Row </button>
         </>
       ) }
     </NewFileInput>
